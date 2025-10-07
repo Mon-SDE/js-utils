33ae3a63560a4d81588b3a11d5e093815563f927
Angkur Mondal <mondal.angkur@gmail.com>
2025-10-07 22:10:21 +0600

    feat: added formatMoney and parseMoney methods functions and test cases.

--- tests/numberUtils.test.ts
+++ tests/numberUtils.test.ts
@@ -1,5 +1,14 @@
-import {describe, expect, it} from 'vitest';
-import { average, clamp, isEven, isOdd, randomInt, sum } from "../src";
+import { describe, expect, it } from "vitest";
+import {
+  average,
+  clamp,
+  formatMoney,
+  isEven,
+  isOdd,
+  parseMoney,
+  randomInt,
+  sum,
+} from "../src";
 
 describe("number-utils", () => {
   it("should check even/odd correctly", () => {
@@ -30,5 +39,27 @@ describe("number-utils", () => {
     expect(result).toBeGreaterThanOrEqual(1);
     expect(result).toBeLessThanOrEqual(10);
     expect(Number.isInteger(result)).toBe(true);
+  });
+
+  it("should format currency correctly", () => {
+    expect(formatMoney(1234567.89)).toBe("1,234,567.89");
+    expect(formatMoney(10000)).toBe("10,000.00");
+    expect(formatMoney(98776.4, 1)).toBe("98,776.4");
+    expect(formatMoney(-98776.4)).toBe("-98,776.40");
+    expect(formatMoney(0)).toBe("0.00");
+    expect(formatMoney(0.1 + 0.2)).toBe("0.30");
+    expect(() => formatMoney("1234" as any)).toThrow("Invalid number provided");
+  });
+
+  it("should parse currency correctly", () => {
+    expect(parseMoney("1,234,567.89")).toBe(1234567.89);
+    expect(parseMoney("98,765.4")).toBe(98765.4);
+    expect(parseMoney("")).toBe(0);
+    expect(() => parseMoney("abc")).toThrow("Invalid currency format: abc");
+    expect(parseMoney("98.40 Dollars")).toBe(98.4);
+    expect(parseMoney("-123,498.40 Dollars")).toBe(-123498.4);
+    expect(parseMoney("-0.50")).toBe(-0.5);
+    expect(parseMoney("$1,234.50")).toBe(1234.5);
+    expect(parseMoney("1 234.50")).toBe(1234.5);
   });
 });
